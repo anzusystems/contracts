622152fb1e9ffbbc42754f455af97e7abb75f22d
sakulb <lukas.budos@gmail.com>
2022-12-27 13:03:16 +0100

    Renamed OwnersAwareInterface.

--- src/Entity/Interfaces/OwnersAwareInterface.php
+++ src/Entity/Interfaces/OwnersAwareInterface.php
@@ -7,7 +7,7 @@ namespace AnzuSystems\Contracts\Entity\Interfaces;
 use AnzuSystems\Contracts\Entity\AnzuUser;
 use Doctrine\Common\Collections\Collection;
 
-interface OwnersHavingInterface
+interface OwnersAwareInterface
 {
     /**
      * @return Collection<int, AnzuUser>
